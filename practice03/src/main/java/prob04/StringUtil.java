eb988c7c6d1528e0d442d37103979f340e210b91
Sunjae <nalalasjsj@gmail.com>
2022-01-18 00:50:14 +0900

    #prob03-4

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,12 @@
 package prob04;
 
 public class StringUtil {
+	public static String concatenate(String[] array) {
+		String result ="";
+		int num = array.length;
+		for(int i=0; i<num; i++) {
+			result += array[i];
+		}
+		return result;
+	}
 }
